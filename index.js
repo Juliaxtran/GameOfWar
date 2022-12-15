90629e4de218802b60441f8f0af3fca477fff5eb
Julia Tran <julia.kim.tran@hotmail.com>
2022-12-15 17:16:15 -0500

    Save Deck Id

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+let deckId = null;
+
  const handleClick = () => {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        console.log(data);
+        let deckId = data.deck_id;
+        console.log(deckId);
+      })
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
