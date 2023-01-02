c8918633e1a29cb34f41e7ed389446e262d5db98
Julia Tran <julia.kim.tran@hotmail.com>
2023-01-02 17:32:30 -0500

    Display card images

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let deckId
-
+let cardOne = document.getElementById("card-one")
+let cardTwo = document.getElementById("card-two")
 
 const handleClick = () => {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -10,16 +11,15 @@ const handleClick = () => {
     })
 }
 
-
 document.getElementById("new-deck").addEventListener("click", handleClick)
 
 document.getElementById("draw-cards").addEventListener("click", () => {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        cardOne.innerHTML = `<img src="${data.cards[0].image}">`
+        cardTwo.innerHTML = `<img src="${data.cards[1].image}">`
+      })
 })
-
-
-
 
 
